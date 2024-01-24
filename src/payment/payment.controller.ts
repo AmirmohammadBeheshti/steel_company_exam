6ac7814d370e3b32d6e97105dc7fe19675a0afb9
Amirmohammad Beheshti <amirmohammad.beheshti@gmail.com>
2024-01-24 16:17:16 +0330

    monopay

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { getPaymentDriver } from 'monopay';
 import { lastValueFrom } from 'rxjs';
 
 @ApiTags('Payment')
@@ -20,5 +21,12 @@ export class PaymentController {
 
     console.log(a);
     return a.data;
+  }
+
+  @Post('start')
+  async start(@Body() payload: {}) {
+    const driver = getPaymentDriver('saman') as any;
+    const paymentInfo = await driver.requestPayment(payload);
+    return paymentInfo;
   }
 }
