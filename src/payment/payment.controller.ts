f0b5931a55dd7eded1d6d2eb1d1b9cfb77f0d5cb
Amirmohammad Beheshti <amirmohammad.beheshti@gmail.com>
2024-01-24 10:30:38 +0330

    controller

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,15 +1,24 @@
 import { HttpService } from '@nestjs/axios';
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { lastValueFrom } from 'rxjs';
 
 @ApiTags('Payment')
 @Controller('payment')
 export class PaymentController {
   constructor(private readonly httpService: HttpService) {}
+
   @Post()
-  async post(@Body() payload: any) {
-    return await this.httpService
-      .post('https://sep.shaparak.ir/OnlinePG/OnlinePG', payload)
-      .toPromise();
+  async post(@Body() payload: {}) {
+    console.log(payload);
+    const a = await lastValueFrom(
+      this.httpService.post(
+        'https://sep.shaparak.ir/OnlinePG/OnlinePG',
+        payload,
+      ),
+    );
+
+    console.log(a);
+    return true;
   }
 }
